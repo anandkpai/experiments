3083541a073e48dc73e0b4423859913e78124529
Anand Pai <anandkpai@gmail.com>
2023-05-21 18:27:12 -0400

    removing gcp k jenkins

--- kubernetes-jenkins/docker/groovy_scripts/disable_cli_remoting.groovy
+++
@@ -1,5 +0,0 @@
-import jenkins.model.*
-
-def instance = Jenkins.getInstance()
-jenkins.model.Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
-instance.save()
--- kubernetes-jenkins/docker/groovy_scripts/slave_to_master_access_control.groovy
+++
@@ -1,6 +0,0 @@
-import jenkins.security.s2m.AdminWhitelistRule
-import jenkins.model.Jenkins
-
-def instance = Jenkins.instance
-Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
-instance.save()
